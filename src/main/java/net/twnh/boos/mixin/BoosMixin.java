a4475cb6827b34110fe19922efd6a7776b926961
Ted Laesch <65507895+Atti1a-dev@users.noreply.github.com>
2022-04-12 13:13:34 -0500

    - Added model to test_item - Added custom class to test_item - Added custom ItemGroup

--- src/main/java/net/twnh/boos/mixin/BoosMixin.java
+++ src/main/java/net/twnh/boos/mixin/BoosMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class BoosMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		Boos.LOGGER.info("This line is printed by an example mod mixin!");
+		Boos.LOGGER.info("This line is printed by the BOOS mod mixin!");
 	}
 }
